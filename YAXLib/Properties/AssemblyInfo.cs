7301278d4480eac8fc0d16fd6bc01bbbd135a61d
Sina Iravanian <sina.iravanian@gmail.com>
2012-06-11 21:52:04 +1000

    YAXLib is now an strongly named assembly!

--- YAXLib/Properties/AssemblyInfo.cs
+++ YAXLib/Properties/AssemblyInfo.cs
@@ -32,7 +32,12 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.8.0.0")]
-[assembly: AssemblyFileVersion("2.8.0.0")]
+[assembly: AssemblyVersion("2.8.2.0")]
+[assembly: AssemblyFileVersion("2.8.2.0")]
 
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("YAXLibTests")]
+[assembly: InternalsVisibleTo("YAXLibTests, PublicKey=" + 
+    "0024000004800000940000000602000000240000525341310004000001000100d7f87eefee568e" +
+    "19ff867da48567811d16853dbc045adcae7a9682012ad27a39dd24c9959db4e87965a589859f8a" +
+    "2cfc33c2d4997bf969dc4baa159ceefe1a85f45368d76918c764df03763848f50370e660da01bb" +
+    "4b2614fb67f8d0f7183c0c640b16c61d47628d8e4ba20f86dc6bab11b0528425efd05607c28ac5" +
+    "76f92ca1")]
