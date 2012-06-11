c433b06aaa33e7216fd2b5dbaf10ca28495dc45e
Sina Iravanian <sina.iravanian@gmail.com>
2012-06-11 21:46:04 +1000

    Removing the sample classes from demo-application, and using those in the test-project

--- DemoApplication/FormMain.cs
+++ DemoApplication/FormMain.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows.Forms;
 using YAXLib;
-using DemoApplication.SampleClasses;
+using YAXLibTests.SampleClasses;
 
 namespace DemoApplication
 {
