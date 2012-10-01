c0ce20c1f8de6643472a007457b08f84ac20c9ca
Sina Iravanian <sina.iravanian@gmail.com>
2012-10-02 00:11:46 +1000

    Updating ChangeLog Increasing Version to 2.10

--- YAXLibTests/Properties/AssemblyInfo.cs
+++ YAXLibTests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("YAXLibTests")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
