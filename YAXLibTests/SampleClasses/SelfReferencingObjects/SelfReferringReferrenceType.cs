aec1fda6a2888783d7995ccf1e77807ae4c54053
Sina Iravanian <sina.iravanian@gmail.com>
2014-04-03 12:17:44 +1100

    Added a Self Referring Sample class that will lead to stack overflow exception upon serialization

---
+++ YAXLibTests/SampleClasses/SelfReferencingObjects/SelfReferringReferrenceType.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace YAXLibTests.SampleClasses.SelfReferencingObjects
+{
+    [ShowInDemoApplication(SortKey = "_")]
+    public class SelfReferringReferrenceType
+    {
+        public string ParentDescription { get; set; }
+        public ChildReferrenceType Child { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(ParentDescription);
+            sb.Append("|->   ").Append(Child).AppendLine();
+            return sb.ToString();
+        }
+
+        public static SelfReferringReferrenceType GetSampleInstance()
+        {
+            var parent = new SelfReferringReferrenceType
+            {
+                ParentDescription = "I'm Parent",
+            };
+            
+            
+            var child = new ChildReferrenceType
+            {
+                ChildDescription = "I'm Child",
+                Parent = parent
+            };
+
+            parent.Child = child;
+            return parent;
+        }
+    }
+
+    public class ChildReferrenceType
+    {
+        public string ChildDescription { get; set; }
+        public SelfReferringReferrenceType Parent { get; set; }
+
+        public override string ToString()
+        {
+            return ChildDescription;
+        }
+    }
+}
