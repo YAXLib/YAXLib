4f8ad0a76f25377c1f37065f84e234b68d77594b
Sina Iravanian <sina.iravanian@gmail.com>
2014-05-12 22:48:47 +1000

    Added an example for infinite loop with calculated properties.

---
+++ YAXLibTests/SampleClasses/SelfReferencingObjects/CalculatedPropertiesCanCauseInfiniteLoop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace YAXLibTests.SampleClasses.SelfReferencingObjects
+{
+    [ShowInDemoApplication(SortKey = "_")]
+    public class CalculatedPropertiesCanCauseInfiniteLoop
+    {
+        public decimal Data { get; set; }
+
+        public CalculatedPropertiesCanCauseInfiniteLoop Reciprocal
+        {
+            get
+            {
+                if (Data == 0M)
+                    return null;
+
+                decimal reciprocal = 1.0M/Data;
+                return new CalculatedPropertiesCanCauseInfiniteLoop {Data = reciprocal};
+            }
+        }
+
+        public static CalculatedPropertiesCanCauseInfiniteLoop GetSampleInstance()
+        {
+            return new CalculatedPropertiesCanCauseInfiniteLoop{Data = 2.0M};
+        }
+
+        public override string ToString()
+        {
+            return GeneralToStringProvider.GeneralToString(this);
+        }
+    }
+}
