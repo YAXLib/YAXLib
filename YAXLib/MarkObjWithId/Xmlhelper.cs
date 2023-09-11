3f6dd147db8a2787c46af6452885832987c45e4c
strugglesharp <strugglesharp@gmail.com>
2023-09-11 16:48:23 +0800

    对实例标记ObjId, 实现对循环引用或多处引用的正反序列化 For instance tag ObjId, realize the  serialization of circular reference or multiple references

---
+++ YAXLib/MarkObjWithId/Xmlhelper.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (C) Sina Iravanian, Julian Verdurmen, axuno gGmbH and other contributors.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace YAXLib.MarkObjWithId;
+
+internal static class Xmlhelper
+{
+
+
+    public static string ATTR_FLAG_OBJID => DictObjWithId.ATTR_FLAG_OBJID;
+    /// <summary>
+    /// Only the repeated ObjId is keept
+    /// </summary>
+    /// <param name="mainDocument"></param>
+    /// <returns></returns>
+    internal static XDocument ClearUnNecessaryObjId(this XDocument mainDocument)
+    {
+        var root = mainDocument.Root;
+        var ItemArr = CollectItems(root, new List<ObjIdItem>());
+        var group = ItemArr.GroupBy(x => x.ObjId, (idx, parents) => (ID: idx, CNT: parents.Count())).ToList();
+        var IdToDelArr = group.Where(x => x.CNT == 1).Select(x => x.ID).ToList();
+        ItemArr.Where(item => IdToDelArr.Exists(id => id == item.ObjId))
+            .ToList()
+            .ForEach(item => item.Parent.Attribute(ATTR_FLAG_OBJID).Remove());
+
+        return mainDocument;
+
+
+        List<ObjIdItem> CollectItems(XElement xelement, List<ObjIdItem> ItemArr)
+        {
+            var attr = xelement.Attribute(ATTR_FLAG_OBJID);
+            if (attr != null)
+                ItemArr.Add(new ObjIdItem { Parent = xelement, ObjId = int.Parse(attr.Value) });
+            foreach (var xe in xelement.Elements())
+                ItemArr = CollectItems(xe, ItemArr);
+            return ItemArr;
+        }
+    }
+
+    class ObjIdItem
+    {
+        public int ObjId { get; set; }
+        public XElement Parent { get; set; }
+
+    }
+
+
+    static public bool ShouldMarkObjId(this MemberWrapper member)
+    {
+        var memtype = member.MemberType;
+        if (memtype.IsValueType)
+            return false;
+        if (!member.IsSerializedAsElement)
+            return false;
+
+        if (memtype.FullName == "System.String")
+            return false;
+        return true;
+
+    }
+    static public bool ShouldMarkObjId(this Type memtype)
+    { 
+        if (memtype.IsValueType)
+            return false; 
+
+        if (memtype.FullName == "System.String")
+            return false;
+        return true;
+
+    }
+
+    public static int? GetObjIdFromAttribute(this XElement? xe)
+    {
+        var attr = xe?.Attribute(DictObjWithId.ATTR_FLAG_OBJID);
+        if (attr == null)
+        {
+            return null;
+        }
+        var rst = Int32.Parse(attr.Value);
+        return rst;
+    }
+
+    public static bool IsNullXmlNode(this XElement xe)
+    {
+        var attributCnt = xe.Attributes().Select(x => x.Name.LocalName)
+            .Except(new List<string> { ATTR_FLAG_OBJID }).Count();
+        var isEmpty = xe.IsEmpty;
+        var HasElements = xe.HasElements;
+
+        return attributCnt == 0 && isEmpty && !HasElements;
+    }
+}
+
---
+++ YAXLibTests/MarkObjId2AvertSefRef.cs
@@ -0,0 +1,229 @@
+﻿// Copyright (C) Sina Iravanian, Julian Verdurmen, axuno gGmbH and other contributors.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using YAXLib;
+using YAXLib.Options;
+using static YAXLibTests.CustomTypeInspectorTests;
+
+namespace YAXLibTests;
+[TestFixture]
+public class MarkObjId2AvertSefRef01
+{
+    #region Test01
+    public class CPack
+    {
+        public class CBoxA
+        {
+            public int Int02 { get; set; } = 333;
+            public string Name { get; set; }
+            public CBoxB Child { get; set; }
+        }
+        public class CBoxB
+        {
+            public int Int01 { get; set; } = 444;
+            public string Name { get; set; }
+            public CBoxC Child { get; set; }
+        }
+        public class CBoxC
+        {
+            public int Int02 { get; set; } = 555;
+            public string Name { get; set; }
+            public CBoxA Child { get; set; }
+        }
+        public int Int01 { get; set; } = 666;
+        public string Name { get; set; }
+        public CBoxA ItWillLoop { get; set; }
+        public static CPack GetSampleInstance()
+        {
+            CBoxA boxa = new() { Name = "AAA" };
+            CBoxB boxb = new() { Name = "BBB" };
+            CBoxC boxc = new() { Name = "CCC" };
+            boxa.Child = boxb;
+            boxb.Child = boxc;
+            boxc.Child = boxa;
+            return new CPack { Name = "PackLoop", ItWillLoop = boxa };
+        }
+    }
+    [Test]
+    public void Se01()
+    {
+        var result = """
+            <CPack>
+              <Int01>666</Int01>
+              <Name>PackLoop</Name>
+              <ItWillLoop _ObjId="1000">
+                <Int02>333</Int02>
+                <Name>AAA</Name>
+                <Child>
+                  <Int01>444</Int01>
+                  <Name>BBB</Name>
+                  <Child>
+                    <Int02>555</Int02>
+                    <Name>CCC</Name>
+                    <Child _ObjId="1000" />
+                  </Child>
+                </Child>
+              </ItWillLoop>
+            </CPack>
+            """;
+
+
+        var serializer = new YAXSerializer<CPack>(
+        new SerializerOptions
+        {
+            SerializationOptions = YAXLib.Enums.YAXSerializationOptions.MarkObjId2AvertSefRef
+        });
+
+
+        var got = serializer.Serialize(CPack.GetSampleInstance());
+        Console.WriteLine("got:" + got);
+        Console.WriteLine("result:" + result);
+        Assert.AreEqual(got, result);
+    }
+    [Test]
+    public void De01()
+    {
+        var xmlInput = """
+            <CPack>
+              <Int01>666</Int01>
+              <Name>PackLoop</Name>
+              <ItWillLoop _ObjId="1000">
+                <Int02>333</Int02>
+                <Name>AAA</Name>
+                <Child>
+                  <Int01>444</Int01>
+                  <Name>BBB</Name>
+                  <Child>
+                    <Int02>555</Int02>
+                    <Name>CCC</Name>
+                    <Child _ObjId="1000" />
+                  </Child>
+                </Child>
+              </ItWillLoop>
+            </CPack>
+            """;
+
+
+        var serializer = new YAXSerializer<CPack>(
+            new SerializerOptions
+            {
+                SerializationOptions = YAXLib.Enums.YAXSerializationOptions.MarkObjId2AvertSefRef
+            });
+
+
+        var got = serializer.Deserialize(xmlInput);
+        Console.WriteLine(got);
+        Assert.AreSame(got.ItWillLoop, got.ItWillLoop.Child.Child.Child);
+    }
+
+    #endregion Test01
+
+    public class CPack02
+    {
+        public class CBox
+        {
+            public string Name { get; set; }
+        }
+
+        public List<CBox> BoxList { get; set; }
+
+
+        public static CPack02 GetSampleInstance()
+        {
+            var newone = new CPack02() { BoxList = new List<CBox>() };
+
+            for (int i = 0; i < 5; i++)
+            {
+                newone.BoxList.Add(new CBox
+                {
+                    Name = $"Node{i}",
+                });
+            }
+
+            newone.BoxList[0] = newone.BoxList[2];
+            newone.BoxList[4] = newone.BoxList[2];
+
+            return newone;
+        }
+
+
+    }
+
+    [Test]
+    public void Se02()
+    {
+        var result = """
+         <CPack02>
+           <BoxList>
+             <CBox _ObjId="1000">
+               <Name>Node2</Name>
+             </CBox>
+             <CBox>
+               <Name>Node1</Name>
+             </CBox>
+             <CBox _ObjId="1000" />
+             <CBox>
+               <Name>Node3</Name>
+             </CBox>
+             <CBox _ObjId="1000" />
+           </BoxList>
+         </CPack02>
+         """;
+
+
+        var serializer = new YAXSerializer<CPack02>(
+        new SerializerOptions
+        {
+            SerializationOptions = YAXLib.Enums.YAXSerializationOptions.MarkObjId2AvertSefRef
+        });
+
+
+        var got = serializer.Serialize(CPack02.GetSampleInstance());
+        Console.WriteLine("got:" + got);
+        Console.WriteLine("result:" + result);
+        Assert.AreEqual(got, result);
+    }
+    [Test]
+    public void De02()
+    {
+        var xmlInput = """
+            <CPack02>
+              <BoxList>
+                <CBox _ObjId="1000">
+                  <Name>Node2</Name>
+                </CBox>
+                <CBox>
+                  <Name>Node1</Name>
+                </CBox>
+                <CBox _ObjId="1000" />
+                <CBox>
+                  <Name>Node3</Name>
+                </CBox>
+                <CBox _ObjId="1000" />
+              </BoxList>
+            </CPack02>
+            """;
+
+
+        var serializer = new YAXSerializer<CPack02>(
+            new SerializerOptions
+            {
+                SerializationOptions = YAXLib.Enums.YAXSerializationOptions.MarkObjId2AvertSefRef
+            });
+
+
+        var got = serializer.Deserialize(xmlInput);
+        Console.WriteLine(got);
+
+        Assert.AreSame(got.BoxList[0], got.BoxList[2]);
+        Assert.AreSame(got.BoxList[4], got.BoxList[2]);
+    }
+}
